a9275ea89c2fe05276d75041cb95dd8f85a90f8c
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-02-14 00:39:28 -0800

    did a function that works kind fo like a factory to create all my DOMS and then i appended all of them in another function which will run when the user wants to go to the home tab

--- src/assets/pages/home.js
+++ src/assets/pages/home.js
@@ -3,15 +3,53 @@ import {content} from '../functions/content';
 import { removeChildren } from '../functions/removeChildren';
 
 
+// function createDiv(){
+//     document.createElement('div');
+// }
 
-const overlay = document.createElement('div');
-overlay.classList.add('overlay');
-const heroTitle = document.createElement('div');
-heroTitle.id = "hero-title";
-heroTitle.textContent = "Discover the soul of Seoul's cooking";
+function createElement(element, id, className, textContent){
+    const createdElement = document.createElement(element);
+    createdElement.id = id;
+    createdElement.classList.add(className);
+    createdElement.textContent = textContent;
+
+    return createdElement
+
+
+}
+
+const overlay = createElement('div', null, 'overlay', null );
+const heroTitle= createElement('h2','hero-title', null, "Discover the soul of Seoul's cooking");
+const heroSubtitle = createElement('h3','hero-subtitle',null,'Authentic taste and exciting experience');
+const heroText = createElement('p','hero-text',null,'123 nonexistent Avenue, Vancouver');
+const heroButtons = createElement('div',null,'buttons', null);
+const heroBtn1 = createElement('button','hero-btn1','btn','Menu');
+const heroBtn2 = createElement('button','hero-btn2','btns', 'About us');
 
 
 
+
+
+// const overlay = document.createElement('div');
+// overlay.classList.add('overlay');
+// const heroTitle = document.createElement('div');
+// heroTitle.id = "hero-title";
+// heroTitle.textContent = "Discover the soul of Seoul's cooking";
+
+// const heroSubtitle = document.createElement('h2');
+
+
+
+// const buttons = document.createElement('div');
+// buttons.classList.add('buttons');
+
+// const heroBtn1 = document.createElement('button');
+// heroBtn1.id = "hero-btn1";
+// heroBtn1.textContent = "Menu";
+
+// const heroBtn2 = document.createElement('div');
+// heroBtn2.id = "hero-btn2";
+// heroBtn2.textContent = "About us";
 
 
 
@@ -23,12 +61,19 @@ const home = function(){
 removeChildren(); // this will be to reset the whole content section
     console.log('home1');
     console.log(content);
-    content.style.backgroundImage = "../img/samgyeopsal.png";
+    
 
     // appending children
     overlay.appendChild(heroTitle);
+    overlay.appendChild(heroSubtitle);
+    overlay.appendChild(heroText);
+
+    heroButtons.appendChild(heroBtn1);
+    heroButtons.appendChild(heroBtn2);
+    overlay.appendChild(heroButtons);
+
     content.appendChild(overlay);
-    
+    content.style.backgroundImage = "../img/samgyeopsal.png";
 };
 
 
